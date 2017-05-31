f914cee0260bc7e9fe5e4561bf985d8d96cebf76
Burak ÖZKIRDENİZ <burakozkirdeniz@client-204-242.swh.uni-giessen.de>
2017-05-31 09:33:08 +0200

    added ege to index.js

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ bot.onText(/(.+)$/, function (msg, match) {
   let replyOptions = {
       reply_markup: {
           inline_keyboard: [
+              [ { text: "Ege Üniversitesi",  callback_data: "Ege Üniversitesi",  } ],            
               [ { text: "Eskişehir Osmangazi Üniversitesi",  callback_data: "Eskişehir Osmangazi Üniversitesi",  } ],
               [ { text: "Anadolu Üniversitesi",  callback_data: "Anadolu Üniversitesi",  }, { text: "Boğaziçi Üniversitesi",  callback_data: "Boğaziçi Üniversitesi",  } ],
               [ { text: "Dokuz Eylül Üniversitesi",  callback_data: "Dokuz Eylül Üniversitesi",  }, { text: "İstanbul Teknik Üniversitesi",  callback_data: "İstanbul Teknik Üniversitesi",  }  ],
