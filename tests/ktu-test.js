dc5adcd371d0da21a8c81253478b6e44fe55c05e
Mücahid Doğan <mucahid.dogan@ceng.ktu.edu.tr>
2017-06-05 20:37:57 +0300

    Karadeniz Teknik Üniversitesi Test

--- tests/ktu-test.js
+++ tests/ktu-test.js
@@ -1,25 +1,6 @@
 const scrapeIt = require("scrape-it");
+const data = require('../sites/ktu.json')
 
-const data = {
-    "university": "Karadeniz Teknik Üniversitesi",
-    "faculity": "Bilgisayar Mühendisliği",
-    "home":"http://www.ktu.edu.tr/",
-    "url": "http://www.ktu.edu.tr/bilgisayar-tumduyuru",
-    "scrape": {
-        "news": {
-            "listItem": "tr",
-            "name": "announcement",
-            "data": {
-                "url": {
-                    "selector": "a.yaz3.textcolor",
-                    "attr": "href"
-                },
-                "title": "a",
-                "publishedAt": "span.date"
-            }
-        }
-    }
-}
 const baseURI = data.url;
 const scrape = data.scrape;
 
